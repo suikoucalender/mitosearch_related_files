91697ee80082dd51a1165a295975021f90d66561
suikoucalender <79552757+suikoucalender@users.noreply.github.com>
2022-06-22 18:23:59 +0900

    Add files via upload

---
+++ create_input.py
@@ -0,0 +1,235 @@
+# coding: utf-8
+import xlrd
+import sys
+import subprocess
+import os
+
+prefix = sys.argv[1]
+outputFileDirPath = sys.argv[2]
+fishname_ja_Path = sys.argv[3]
+tmpDir = sys.argv[4]
+inputFilePath = tmpDir + "/" + prefix + "/" + prefix + ".input.tmp"
+outputFilePath = outputFileDirPath + "/" + prefix + ".input"
+
+def main():
+    # ファイルが存在しない場合は例外処理
+    if not os.path.exists(inputFilePath):
+        return
+    
+    # input.tmpファイルを開く
+    inputFile = open(inputFilePath)
+
+    # input.tmpファイルの各行をリストとして取得
+    inputFileRowList = inputFile.readlines()
+
+    # 以下の各関数ではinputファイルの改行区切りリストを関数の入力および出力としている。
+
+    # 和名を付与した魚種組成リストを作成
+    inputFileRowList_withJAname = add_ja_name(inputFileRowList)
+    print(inputFileRowList_withJAname)
+
+    # 魚種の重複を排除
+    inputFileRowList_withJAname_duplicationRemoved = duplication_remove(inputFileRowList_withJAname)
+    print(inputFileRowList_withJAname_duplicationRemoved)
+
+    # 組成の中で占める割合が1%未満の種を除去
+    inputFileRowList_withJAname_duplicationRemoved_minorRemoved = minor_remove(inputFileRowList_withJAname_duplicationRemoved)
+    print(inputFileRowList_withJAname_duplicationRemoved_minorRemoved)
+
+    # 100read未満のサンプルを除去および組成を百分率で表示
+    inputFileRowList_withJAname_duplicationRemoved_minorRemoved_percent = calc_percent(inputFileRowList_withJAname_duplicationRemoved_minorRemoved)
+    print(inputFileRowList_withJAname_duplicationRemoved_minorRemoved_percent)
+
+    # 100read未満のサンプルは処理を終了
+    if inputFileRowList_withJAname_duplicationRemoved_minorRemoved_percent == "less than 100 reads":
+        return
+    
+    # inputファイルの書き込み
+    write_inputfile(inputFileRowList_withJAname_duplicationRemoved_minorRemoved_percent)
+
+    inputFile.close()
+
+def add_ja_name(inputFileRowList):
+    # Xlrdモジュールで和名が記載されたExcelファイルを開く
+    fishname_ja_wb = xlrd.open_workbook(fishname_ja_Path)
+    
+    # シートを取得
+    fishname_ja_sh = fishname_ja_wb.sheet_by_index(0)
+    
+    # 学名のリストと和名のリストを取得
+    academic_nameList = fishname_ja_sh.col_values(4)
+    japanese_namesList = fishname_ja_sh.col_values(3)
+    
+    # Excelの長さを取得
+    sh_len = len(academic_nameList)
+
+    # 和名付与済み魚種組成リスト
+    inputFileRowList_withJAname = []
+
+    for i, row in enumerate(inputFileRowList):
+        # Headerを追加
+        if i == 0:
+            inputFileRowList_withJAname.append(row)
+        # 和名に変換
+        else:
+            # 種名を取得
+            species = row.split("\t")[0]
+
+            # 組成量を取得
+            comp = row.split("\t")[1]
+
+            # デフォルトの和名に空文字を割り当てる
+            japanese_name = ""
+
+            # 和名リストの各行に対して検索を行い、ヒットしたものに置換
+            for i in range(sh_len):   
+                academic_name = academic_nameList[i].split()[:2]
+                
+                # 学名にマッチするものを検索
+                if academic_name[0] in species and academic_name[1] in species:
+                    
+                    # マッチした学名に対応する和名を取得
+                    japanese_name = japanese_namesList[i]
+                    break
+            
+            # 種名のみを抽出
+            species = species.split("|")[2]
+            species = species.split("(")[0]
+
+
+            if japanese_name != "":
+                # 和名付与
+                species = japanese_name + ":" + species
+
+            # 新しく書き込む行を作成
+            new_row = species + "\t" + comp
+
+            inputFileRowList_withJAname.append(new_row)
+    
+    return inputFileRowList_withJAname
+
+def duplication_remove(inputFileRowList):
+    # 重複排除済み魚種組成リスト
+    inputFileRowList_duplicationRemoved = []
+
+    merged_comp_obj = {}
+
+    for i, row in enumerate(inputFileRowList):
+        # Headerを追加
+        if i == 0:
+            inputFileRowList_duplicationRemoved.append(row)
+        else:
+            row = row.split("\t")
+            species = row[0]
+            comp = int(row[1].replace("\n",""))
+
+            # 既に魚種組成に存在している場合はマージ
+            if species in merged_comp_obj:
+                merged_comp_obj[species] = merged_comp_obj[species] + comp
+            # 魚種組成に存在していない場合は新規に追加
+            else:
+                merged_comp_obj[species] = comp
+    # 組成量をキーにソート
+    merged_comp_list_sorted = sorted(merged_comp_obj.items(), key=lambda x:x[1], reverse=True)
+
+    # 出力リストに追加
+    for row in merged_comp_list_sorted:
+        new_row = row[0] + "\t" + str(row[1]) + "\n"
+        inputFileRowList_duplicationRemoved.append(new_row)
+
+    return inputFileRowList_duplicationRemoved
+
+def minor_remove(inputFileRowList):
+    # 1%未満の組成の魚種排除した魚種組成リスト
+    inputFileRowList_minorRemoved = []
+
+    # 組成の累計
+    total = 0
+
+    # 累計を算出
+    for i, row in enumerate(inputFileRowList):
+        if i == 0:
+            continue
+        else:
+            row = row.split("\t")
+            
+            # 種名がない場合の例外処理
+            if row[0] == "":
+                continue
+
+            try:
+                total += int(row[1].replace("\n",""))
+            except:
+                # 数値変換できない場合の例外処理
+                continue
+
+    # 1%未満の組成の魚種排除
+    for i, row in enumerate(inputFileRowList):
+        if i == 0:
+            # Headerを追加
+            inputFileRowList_minorRemoved.append(row)
+        else:
+            comp = int(row.split("\t")[1].replace("\n",""))
+
+            # 1%未満の組成の魚種を除去
+            if float(comp) / float(total) < 0.01:
+                continue            
+            
+            inputFileRowList_minorRemoved.append(row)
+    
+    return inputFileRowList_minorRemoved
+
+def calc_percent(inputFileRowList):
+    # 百分率で組成を表示した魚種組成リスト
+    inputFileRowList_percent = []
+
+    # 組成の累計
+    total = 0
+
+    # 累計を算出
+    for i, row in enumerate(inputFileRowList):
+        if i == 0:
+            continue
+        else:
+            row = row.split("\t")
+            
+            # 種名がない場合の例外処理
+            if row[0] == "":
+                continue
+
+            try:
+                total += int(row[1].replace("\n",""))
+            except:
+                # 数値変換できない場合の例外処理
+                continue
+
+    # 100read未満のサンプルは"less than 100 reads"を返す
+    if total < 100:
+        return "less than 100 reads"
+    
+    # 魚種組成を百分率で算出
+    for i, row in enumerate(inputFileRowList):
+        if i == 0:
+            # Headerを追加
+            inputFileRowList_percent.append(row)
+        else:
+            species = row.split("\t")[0]
+            comp = int(row.split("\t")[1].replace("\n",""))
+            comp = round(float(comp) / float(total),4) * 100
+            new_row = species + "\t" + str(comp) + "\n"
+            inputFileRowList_percent.append(new_row)
+    
+    return inputFileRowList_percent
+
+def write_inputfile(inputFileRowList):
+    # 書き込み先のinputファイルを書き込みモードで開く
+    outputFile = open(outputFilePath, "w")
+
+    # 各行を書き込み
+    for row in inputFileRowList:
+        outputFile.write(row.encode('utf-8'))
+
+    outputFile.close()
+
+if __name__ == "__main__":
+    main()
---
+++ create_input.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# SRA番号
+prefix=$1
+
+# ------------
+
+# Workdir
+workdir=/home/yoshitake/mitosearch_update_db
+# Scriptが配置されているディレクトリ
+scriptdir=${workdir}/script
+# 各サンプルの一時ファイルを配置するディレクトリを格納するディレクトリ
+tmpdir=${workdir}/tmp
+# サンプルデータを配置するディレクトリ(各ディレクトリでファイルの重複がないようにする必要がある)
+edna_file_list=(${workdir}/fastq)
+# MitoFishのBlast DBファイル
+blastdb=${workdir}/db/complete_partial_mitogenomes.fa
+# inputファイルの出力先ディレクトリ
+outputFileDirPath=${workdir}/inputFiles
+# 魚種和名ファイルのPath
+fishname_ja_Path=${workdir}/db/20210718_JAFList.xlsx
+# singularityのPath
+singularity_path=/home/yoshitake/tool/singularity-3.5.2/bin/singularity
+# blastnのPATH
+blastn_path=/home/yoshitake/ncbi-blast-2.8.1+/bin/blastn
+
+# ------------
+
+# 一時ファイルを配置するディレクトリをサンプルごとに作成
+mkdir -p ${tmpdir}/${prefix}
+
+for edna_sequence_filedir in ${edna_file_list[@]}
+do
+
+# Pair EndのシーケンスデータはFLASHでマージしたのちに、マージがされなかったForward側のリードを結合する
+if [ -e ${edna_sequence_filedir}/${prefix}_1.fastq.gz -a -e ${edna_sequence_filedir}/${prefix}_2.fastq.gz ]; then
+    cp ${edna_sequence_filedir}/${prefix}_1.fastq.gz ${tmpdir}/${prefix}
+    gunzip ${tmpdir}/${prefix}/${prefix}_1.fastq.gz
+    # FLAShによるペアエンドリードのマージを行った後に、マージに失敗したリードについてはForward側のみをFLAShの出力ファイル(out.extendedFrags.fastq)に追加する。
+    # マージに失敗(ペアエンド間でリード数が異なる場合など)ではForward側のリードファイルを使用する(out.extendedFrags.fastqというファイルにForward側のリード配列を格納)
+    # echo "singularity run --bind ${edna_sequence_filedir}:${edna_sequence_filedir} ${workdir}/singularity_image/flash.sif -d ${tmpdir}/${prefix} -M 300 ${edna_sequence_filedir}/${prefix}_1.fastq.gz ${edna_sequence_filedir}/${prefix}_2.fastq.gz"
+    { { ${singularity_path} run --bind ${edna_sequence_filedir}:${edna_sequence_filedir} ${workdir}/singularity_image/flash.sif -d ${tmpdir}/${prefix} -M 300 ${edna_sequence_filedir}/${prefix}_1.fastq.gz ${edna_sequence_filedir}/${prefix}_2.fastq.gz; } && cat ${tmpdir}/${prefix}/out.notCombined_1.fastq >> ${tmpdir}/${prefix}/out.extendedFrags.fastq; } || cat ${tmpdir}/${prefix}/${prefix}_1.fastq > ${tmpdir}/${prefix}/out.extendedFrags.fastq
+    
+# Single Endのシーケンスデータはそのリードをそのまま使用する
+else
+    if [ -e ${edna_sequence_filedir}/${prefix}_1.fastq.gz ]; then
+        cp ${edna_sequence_filedir}/${prefix}_1.fastq.gz ${tmpdir}/${prefix}
+        gunzip ${tmpdir}/${prefix}/${prefix}_1.fastq.gz
+        mv ${tmpdir}/${prefix}/${prefix}_1.fastq ${tmpdir}/${prefix}/out.extendedFrags.fastq
+
+    fi
+
+    if [ -e ${edna_sequence_filedir}/${prefix}_2.fastq.gz ]; then
+        cp ${edna_sequence_filedir}/${prefix}_2.fastq.gz ${tmpdir}/${prefix}
+        gunzip ${tmpdir}/${prefix}/${prefix}_2.fastq.gz
+        mv ${tmpdir}/${prefix}/${prefix}_2.fastq ${tmpdir}/${prefix}/out.extendedFrags.fastq
+
+    fi
+
+fi
+
+done
+
+# MiFishプライマーのリバース側の相補鎖以降の配列(Adapter Primerも含む)を除去
+${singularity_path} run ${workdir}/singularity_image/cutadapt.sif cutadapt -a CAAACTAGGATTAGATACCCCACTATG -o ${tmpdir}/${prefix}/out.extendedFrags_trimed.fastq ${tmpdir}/${prefix}/out.extendedFrags.fastq
+
+# FASTQからFATSTAファイルに変換
+awk '(NR - 1) % 4 < 2' ${tmpdir}/${prefix}/out.extendedFrags_trimed.fastq | sed 's/@/>/' > ${tmpdir}/${prefix}/out.extendedFrags_trimed.fasta
+
+# MitoFishデータベースに対してBlastnで相同性検索を行う。
+${blastn_path} -num_threads 8 -db ${blastdb} -query ${tmpdir}/${prefix}/out.extendedFrags_trimed.fasta -outfmt "6 qseqid sseqid qlen slen pident length mismatch gapopen qstart qend sstart send evalue bitscore staxids stitle" -max_target_seqs 1 -out ${tmpdir}/${prefix}/blast.result
+
+#Inputファイルのヘッダを書き込み
+echo -e "id\t${prefix}.fastq" > ${tmpdir}/${prefix}/${prefix}.input.tmp
+
+#Inputファイル書き込み
+cat ${tmpdir}/${prefix}/blast.result | awk '$3 > 100' | awk '$5 > 90' | awk '$6 / $3 > 0.9' | cut -f 1,16 | sort | uniq | cut -f 2 | sort | uniq -c | sort -r -n | awk 'BEGIN{OFS="\t"} {c="";for(i=2;i<=NF;i++) c=c $i" "; print c, $1}' >> ${tmpdir}/${prefix}/${prefix}.input.tmp
+
+# inputファイルの修正
+# python2 ${scriptdir}/create_input.py ${prefix} ${outputFileDirPath} ${fishname_ja_Path} ${tmpdir}
+${singularity_path} run --bind ${outputFileDirPath}:${outputFileDirPath} ${workdir}/singularity_image/python_xlrd.sif python ${scriptdir}/create_input.py ${prefix} ${outputFileDirPath} ${fishname_ja_Path} ${tmpdir}
