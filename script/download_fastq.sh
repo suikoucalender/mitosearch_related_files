7410345959ca5d8fe27c8d65b47b3e3d261dfd1b
suikoucalender <suikoucalender@gmail.com>
2022-09-07 13:46:23 +0900

    lat-long取得スクリプト修正

--- script/download_fastq.sh
+++ script/download_fastq.sh
@@ -21,10 +21,34 @@ ${singularity_path} run ${workdir}/singularity_image/sratoolkit.sif fastq-dump $
 
 # 新しく取得したSRA番号のメタデータを取得
 set +x
+
 a=$(curl https://www.ncbi.nlm.nih.gov/biosample/`curl "https://www.ncbi.nlm.nih.gov/sra/?term=$id"|grep SAM|sed 's/SAM/\nSAM/g;'|sed 's/".*//; s/<.*//; s/ .*//'|grep "^SAM"|awk 'NR==1{print $0}'`)
 set +o pipefail
-str="$id"$'\t'`echo "$a"|grep -i lat|grep -i long|sed 's/<[^>]*>/\t/g'|sed 's/\t\+/\n/g'|grep -A 1 long|tail -n 1`$'\t'`echo "$a"|
+lat="$id"$'\t'`echo "$a"|grep -i latitude|sed 's/<[^>]*>/\t/g'|sed 's/\t\+/\n/g'|grep -A 1 latitude|tail -n 1`$'\t'`echo "$a"|
  grep -i "collection date"|sed 's/<[^>]*>/\t/g'|sed 's/\t\+/\n/g'|grep -A 1 -i "collection date"|tail -n 1`
+long="$id"$'\t'`echo "$a"|grep -i longitude|sed 's/<[^>]*>/\t/g'|sed 's/\t\+/\n/g'|grep -A 1 longitude|tail -n 1`$'\t'`echo "$a"|
+ grep -i "collection date"|sed 's/<[^>]*>/\t/g'|sed 's/\t\+/\n/g'|grep -A 1 -i "collection date"|tail -n 1`
+
+str=$lat
+latitude=(`echo $lat`)
+longitude=(`echo $long`)
+
+if [[ $lat == $long ]]; then
+    :
+else
+    if [ `echo "${latitude[1]} > 0.0" | bc` == 1 ]; then
+        lat=${latitude[1]}" N"
+    else
+        lat=`echo ${latitude[1]}| sed 's/^-//'`" S"
+    fi
+
+    if [ `echo "${longitude[1]} > 0.0" | bc` == 1 ]; then
+        long=${longitude[1]}" E"
+    else
+        long=`echo ${longitude[1]}| sed 's/^-//'`" W"
+    fi
+    str=`echo -e ${latitude[0]}'\t'$lat" "$long'\t'${latitude[2]}`
+fi
 set -x
 
 # 同一マシン内での排他制御をしつつ経度緯度を書き込む
